af6bf1338c9ab7f847b84c57b9b2255e3054aaa6
unknown <englederdavid@yahoo.de>
2020-06-14 22:42:10 +0200

    first commit

---
+++ DictionaryUWP/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DictionaryLogic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DictionaryUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DictionaryController dictionaryController = new DictionaryController("\\dictionary.txt");
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private void btnWriteWord_Click(object sender, RoutedEventArgs e)
+        {
+            var germanWord = tbGermanWord.Text;
+
+        }
+    }
+}
